a2fd9c9a97beb9ebedfe0de849a27df723935e41
Hoang Duong Nguyen <hoangduongnguyen.tech@gmail.com>
2022-06-07 22:42:19 +0700

    menu

--- frontend/about_redline.php
+++ frontend/about_redline.php
@@ -4,20 +4,13 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>REDLINE MUSIC PARTY</title>
+    <title>About Redline</title>
     <link rel="stylesheet" href="asset\css\style.css">
 </head>
 <body>
-
     <!-- top -->
     <?php include "menu.html" ?>
-    
-
-    <!-- content -->
-    <img src="asset/img/testimg.png" alt="anh">
-    
-    
-
+    <img src="asset/img/about_redline.png" alt="anh">
     <!-- footer -->
    <?php include "footer.html" ?>
 </body>
