d550209ea2ce5aa372a643c9d4130b8585d9252b
Hoang Duong Nguyen <hoangduongnguyen.tech@gmail.com>
2022-06-07 18:38:12 +0700

    Change to php

--- html/home.php
+++ html/home.php
@@ -5,9 +5,19 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>REDLINE MUSIC PARTY</title>
+    <link rel="stylesheet" href="asset\css\style.css">
 </head>
 <body>
+
+    <!-- top -->
     <h1>REDLINE MUSIC PARTY 2022</h1>
     
+
+    <!-- content -->
+    <img src="asset/img/testimg.png" alt="anh">
+    
+
+    <!-- footer -->
+   <?php include "footer.html" ?>
 </body>
 </html>
