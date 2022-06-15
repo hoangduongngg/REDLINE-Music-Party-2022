ae3313f4f68144c7be4deb333381becc88abc00a
Hoang Duong Nguyen <hoangduongnguyen.tech@gmail.com>
2022-06-15 16:22:39 +0700

    Fix confirm mail

---
+++ frontend/confirm.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width/=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="./confirm/confirm.css">
+	<link rel="stylesheet" href="./confirm/font/fontawesome-free-6.1.1-web/fontawesome-free-6.1.1-web/css/all.min.css">
+</head>
+<body>
+
+    <div class="modal__regis_body">
+            <img src="asset/img/ticket/z3487646832656_d68ad4b29ae5963544b75cc42c5ddad4.jpg" alt="">
+				<div class="modal__info">
+                    <div class="modal__regis__head">
+                        <div class="icon-head">
+                            <i class="modal__regis__form__icon fa-solid fa-circle-check"></i>
+                        </div>
+                        <div class="title-head">
+                            <h3 class="title-head-cf">CHUYỂN KHOẢN THÀNH CÔNG</h3>
+                            <h2 class="title-head-mn">-300.000đ</h2>
+                            <h4 class="title-head-code">Mã giao dịch: 0123456789</h4>
+                        </div>
+    
+                    </div>
+                    <div class="modal__regis__tks-title">
+                        <div class="modal__regis__tks-title-1"><h3>Cảm ơn bạn đã đặt vé,</h3></div>
+                         <div class="modal__regis__tks-title-2">
+                        Vé của bạn đã sẵn sàng. Xin vui lòng xuất trình email này tại cửa soát vé.</div>
+                    </div>  
+                </div> 
+                <h4 class="modal__regis__info-event-title">Thông tin khách hàng
+                </h4>
+				<div class="modal__regis__form modal__info">
+					<div class="modal__regis__form-check">
+                        <img src="./confirm/img/qr.jpg" alt="" class="qr-code">
+                        <div class="modal__regis__form-check-if">
+                            <h4 class="modal__regis__form-seri">1233244453465476756</h4>
+                        <h4 class="modal__regis__form-at">Vui lòng bảo mật số serial và mã code
+                        </h4>
+                        </div>
+                    </div>
+                    <div class="modal__regis__form-elements">
+                    <h5 class="modal__regis__form-sl modal__regis__form-add">Số lượng: 1</h5>
+                    <h5 class="modal__regis__form-name modal__regis__form-add">Chủ sở hữu: NGUYEN HOANG DUONG</h5>
+                    <h5 class="modal__regis__form-time modal__regis__form-add">Thời gian mua vé : 2022 </h5>
+                    <h5 class="modal__regis__form-sr modal__regis__form-add">Serial: xxx xxx xxx</h5>
+                    </div>
+
+				
+
+
+				
+		</div>
+        <img src="asset/img/ticket/z3487646924268_e12f06b90cc07ac2a15330fd08ca5c8d.jpg" alt="">
+    </div>
+
+    <img src="./confirm/img/REDLINE Contact.svg" alt="">
+    <?php include "footer.html" ?>
+</body>
+</html>
